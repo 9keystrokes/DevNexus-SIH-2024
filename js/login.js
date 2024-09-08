c1c840dd834cd54ea035e11a52e7c8c8217a51d5
9keystrokes <168451989+9keystrokes@users.noreply.github.com>
2024-09-08 15:28:10 +0530

    Update login.js

--- js/login.js
+++ js/login.js
@@ -2,43 +2,25 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
-
 mongoose.connect('mongodb+srv://1nayanmandal:mongoatlas@login-backend.orqrz.mongodb.net/?retryWrites=true&w=majority&appName=login-backend', {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
-
 const User = require('./users_schema');
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 app.post('/login', async (req, res) => {
   const { username, password } = req.body;
-  console.log('Login attempt:', { username, password });
-
-  try {
     const user = await User.findOne({ username });
-    console.log('User found:', user);
     if (!user) {
       return res.status(401).send({ message: 'User does not exist.' });
     }
-
     const isValidPassword = await bcrypt.compare(password, user.password);
-    console.log('Password valid:', isValidPassword);
     if (!isValidPassword) {
       return res.status(401).send({ message: 'Invalid password' });
     }
-
     return res.send({ message: 'Login successful' });
-  }
-
-  catch (error){
-    console.error('Error during login:', error);
-    return res.status(500).send({ message: 'An error occurred. Please try again later.' });
-  }
 });
-
 const port = 3000;
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
